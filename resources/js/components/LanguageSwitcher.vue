1280fc94a44fb033f3edb7bfb57f94fe873add0f
shahzadali61 <noahdeveloper96@gmail.com>
2025-04-08 14:49:56 +0500

    implement the Eng | Português | 日本語  langauge accurate working no bugs

--- resources/js/components/LanguageSwitcher.vue
+++ resources/js/components/LanguageSwitcher.vue
@@ -7,9 +7,9 @@ const { props } = usePage();
 
 const availableLocales = [
   { code: 'en', name: 'English' },
-  { code: 'es', name: 'Español' },
-  { code: 'fr', name: 'Français' },
-  { code: 'de', name: 'Deutsch' },
+//   { code: 'es', name: 'Español' },
+//   { code: 'fr', name: 'Français' },
+//   { code: 'de', name: 'Deutsch' },
   { code: 'pt', name: 'Português' },
   { code: 'jp', name: '日本語' },
 ];
